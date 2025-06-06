05759e841b799e5ea6dbfacef9ae65e82f0f0b23
Jhorlen Souza Bianor <97685894+JhorlenPitch@users.noreply.github.com>
2025-06-06 10:10:59 -0400

    Update app.py
    Correção Porta Main

--- app.py
+++ app.py
@@ -32,4 +32,6 @@ def formulario():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
